f78e314ba4a7dff3b62100690bc03b0a12b23e43
xing <xingli@usc.edu>
2017-11-05 22:37:27 -0800

    Implement Trie

---
+++ java/dataStructure/Trie.java
@@ -0,0 +1,55 @@
+package java.dataStructure;
+
+class Trie {
+    TrieNode root;
+    /** Initialize your data structure here. */
+    public Trie() {
+        root = new TrieNode(false);
+    }
+    
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        TrieNode cur = root;
+        for(char c : word.toCharArray()) {
+            if(cur.children[c - 'a'] == null) {
+                cur.children[c - 'a'] = new TrieNode(false);
+            }
+            cur = cur.children[c - 'a'];
+        }
+        cur.isWord = true;
+    }
+    
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        TrieNode cur = root;
+        for(char c : word.toCharArray()) {
+            if(cur.children[c - 'a'] == null) {
+                return false;
+            }
+            cur = cur.children[c - 'a'];
+        }
+        return cur.isWord;
+        
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        TrieNode cur = root;
+        for(char c : prefix.toCharArray()) {
+            if(cur.children[c - 'a'] == null) {
+                return false;
+            }
+            cur = cur.children[c - 'a'];
+        }
+        return true;
+    }
+}
+
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
